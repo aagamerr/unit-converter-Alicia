ecb7612e1fc42ae42f217bb6c7a1151ea3ec4256
aagamerr <aliciaapriliani003@gmail.com>
2026-02-03 10:00:09 +0700

    Added time and temperature converter

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "Converter.h"
 #include "Length.h"
+#include "Time.h"
+#include "Temperature.h"
 
 #include <iostream>
 #include <string>
@@ -16,6 +18,30 @@ int main(){
     double result = myconverter.fromBase(baseVal, "ft");
 
     std::cout << "Value in ft : " << result << std::endl;
+
+    std::string input;
+    std::cout << "Enter value in hour: ";
+    
+    std::cin >> input;
+    double val = std::stod(input);
+
+    Time time_converter;
+    double baseVal = time_converter.toBase(val, "hour");
+    double result =time_converter.fromBase(baseVal, "sec");
+
+    std::cout << "Value in sec : " << result << std::endl;
+
+    std::string input;
+    std::cout << "Enter value in Celcius: ";
+    
+    std::cin >> input;
+    double val = std::stod(input);
+
+    Temperature temp_converter;
+    double baseVal = temp_converter.toBase(val, "Celcius");
+    double result = temp_converter.fromBase(baseVal, "Farenheit");
+
+    std::cout << "Value in Farenheit : " << result << std::endl;
 
 
     return 0;
