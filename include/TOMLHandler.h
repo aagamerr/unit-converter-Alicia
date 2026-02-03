6c238511c6404f14d68ca51da284a772142c2817
aagamerr <aliciaapriliani003@gmail.com>
2026-02-04 05:58:27 +0700

    Added TOMLHandler

---
+++ include/TOMLHandler.h
@@ -0,0 +1,24 @@
+#ifndef TOMLHANDLER_H
+#define TOMLHANDLER_H
+
+#include "FileHandler.h"
+#include <sstream>
+#include <iostream>
+
+class TOMLHandler : public FileHandler {
+private:
+    using Table = std::vector<std::vector<std::string>>;
+    Table table;
+
+public:
+    bool load(std::string filename) override;
+
+    // Selector: CSV column number
+    std::vector<std::string> getColumn(std::string selector) override ;
+
+    void appendColumn(std::string newHeader, const std::vector<std::string>& values) override;
+
+    void save(std::string filename) override;
+};
+
+#endif
